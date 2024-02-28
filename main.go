14aff67ca694bd996dc667441851a0e2b46589ed
theoreotm <qwertzybad@gmail.com>
2024-02-28 19:05:51 +0500

    feat: app directory and health func

--- main.go
+++ main.go
@@ -6,9 +6,15 @@ func main() {
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./app"))))
+	mux.HandleFunc("/healthz", readinessHandler)
 	http.ListenAndServe(":8080", corsMux)
+}
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
 
 func middlewareCors(next http.Handler) http.Handler {
